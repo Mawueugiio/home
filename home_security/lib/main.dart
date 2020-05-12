6bc49ddeb9137f4e0a113c9751ebe2cd840f5699
Oscar Uwayo <oscaruwayo@gmail.com>
2020-05-12 22:14:56 +0200

    With face recognition model

---
+++ home_security/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+FirebaseMessaging _firebaseMessaging=FirebaseMessaging(); 
+
+
+void main(){
+
+runApp(
+  HomePage()
+);
+
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // @override
+  void onState(){
+        
+      // super.onState(); 
+      setupNotification();
+      
+
+        
+  }
+          
+        
+  void setupNotification() async{ 
+    _firebaseMessaging.getToken().then((token){
+        print(token); 
+    });
+        
+    _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async{
+          print("Message: $message"); },
+        onResume: (Map<String,dynamic> message) async{
+          print("Message : $message"); },
+        onLaunch: (Map<String, dynamic> message) async{
+          print("Message: $message");}
+          ); 
+  }
+ 
+        
+          @override
+          Widget build(BuildContext context) {
+            
+            return MaterialApp(
+               
+                home: Scaffold(
+                backgroundColor: Color(0xff00BCD1),
+                appBar: AppBar(
+                title: Text('Home Security'),),
+                body:Center(
+                  child:RaisedButton(
+                    child:Text("Click"),
+                    onPressed: (){},
+                  ),
+                ),
+              ),
+            );
+          }
+        }
+       
+        
+        
+        
+        
